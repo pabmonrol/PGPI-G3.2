bdc9e3bc1cac8a43575bc88c612eb3c1d47fc166
Yesica Garate <yesgarfue@alum.us.es>
2024-12-01 13:26:42 +0100

    Correción en DATABASES, quitar la modificación anterior

--- Django/hundidos/ecommerce/settings.py
+++ Django/hundidos/ecommerce/settings.py
@@ -102,7 +102,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
